861d5feaa1fd97919c2bdbb78a65f60d2a89e28c
Spencer Sablan <spencertsablan@gmail.com>
2021-06-02 21:30:10 -0700

    Added  active users sidebar

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,16 +6,17 @@ const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
+const $sidebar = document.querySelector('#sidebar')
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 // Options
 const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix: true})
 
 socket.on('message', (message) => {
-    console.log(message)
     const html = Mustache.render(messageTemplate, {
         username: message.username,
         message: message.text,
@@ -34,13 +35,19 @@ socket.on('locationMessage', (message) => {
     $messages.insertAdjacentHTML('beforeend',html)
 })
 
+socket.on('roomData', ({room,users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    $sidebar.innerHTML = html
+})
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     $messageFormButton.setAttribute('disabled', 'disabled')
    
-
     const message = document.querySelector('#message-input').value
 
     socket.emit('sendMessage', message, (error) => {
